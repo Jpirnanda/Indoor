653bf1a5949254acd883b45a5901215de13b3679
Jay P <jp.miranda.ga@gmail.com>
2023-03-31 16:07:50 -0300

    Changes

--- LoginLayout/App.js
+++ LoginLayout/App.js
@@ -20,10 +20,19 @@ import SvgQRCode from 'react-native-svg'
 import Checkbox from 'expo-checkbox'
 import { Calendar, CalendarList, Agenda, LocaleConfig } from 'react-native-calendars';
 
-Blue = '#31A9ED'
+// # ----------------- Cores do Projeto -----------------
+const Blue = '#31A9ED'
+const Cyan = "#bdf8ff"
+const baseCyan = "#9edfe7"
+const Yellow = "#f8f4b3"
+const baseYellow = "#faf282"
+const Red = "#ffafc3"
+const baseRed = 'ff7497'
 
 const Tab = createBottomTabNavigator()
 const Stack = createNativeStackNavigator()
+
+// # ----------------- JSONS Simulados -----------------
 
 const screens = [
   {
@@ -69,6 +78,64 @@ const screens = [
     imagePath: require('./assets/screen.png'),
   },
 ]
+
+const content = [
+  {
+    id: 1,
+    title: 'Corolla 0km',
+    dataInicial: '2023-03-04',
+    dataFinal: '2023-03-06',
+    png: require('./assets/mediaIcon.png')
+  },
+  {
+    id: 2,
+    title: 'Kinto Fleet',
+    dataInicial: '05/03/2023',
+    dataFinal: '06/03/2023',
+    png: require('./assets/mediaIcon.png')
+  }
+]
+
+// # ----------------- Funções Gerais -----------------
+
+// Geração de Código para QR
+const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+function generateString(length) {
+  let result = ' ';
+  const charactersLength = characters.length;
+  for (let i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+
+  return result;
+}
+
+// Calendário em PT-BR
+LocaleConfig.locales['br'] = {
+  monthNames: [
+    'Janeiro',
+    'Fevereiro',
+    'Março',
+    'Abril',
+    'Maio',
+    'Junho',
+    'Julho',
+    'Agosto',
+    'Setembro',
+    'Outubro',
+    'Novembro',
+    'Dezembro'
+  ],
+  monthNamesShort: ['Jan.', 'Fev.', 'Mar', 'Abril', 'Mai.', 'Jun.', 'Jul.', 'Ago', 'Set.', 'Out.', 'Nov.', 'Dez.'],
+  dayNames: ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado'],
+  dayNamesShort: ['Dom.', 'Seg.', 'Ter.', 'Quar.', 'Qui.', 'Sex.', 'Sab.'],
+  today: "Hoje"
+};
+LocaleConfig.defaultLocale = 'br';
+
+// 
+
+// # ----------------- Core Functions -----------------
 
 function TabNav() {
   return (
@@ -247,10 +314,16 @@ const styles = StyleSheet.create({
   },
 
   // AgendaTab
+  dayCardCont: {
+    flex: 1,
+    flexDirection: 'column',
+    marginBottom: 20,
+  },
+
   dayCard: {
     flex: 1,
     flexDirection: 'row',
-    marginBottom: 15,
+    marginBottom: 10,
   },
 
   dayDate: {
@@ -275,54 +348,6 @@ const styles = StyleSheet.create({
 })
 
 // # ----------------- Telas Iniciais -----------------
-
-{// const HomeScreen = ({ navigation }) => {
-  //   return (
-
-  //     <View style={styles.container}>
-  //       <ImageBackground source={require('./assets/bg.png')} style={styles.backgroundImage} >
-
-  //         {/* Aqui entra a logo */}
-  //         <View style={styles.container}><Image source={require('./assets/logo.png')} style={{ width: "85%", height: "20%", marginTop: 30 }} /></View>
-
-  //         {/* Aqui entram os inputs */}
-  //         <View style={styles.container}>
-
-  //           <Pressable style={styles.signUpButton} backgroundColor="#FCFBF9" onPress={() => navigation.navigate('Home3')}>
-  //             <View pointerEvents="none">
-  //               <TextInput
-  //                 style={styles.TextInput}
-  //                 placeholder="Cadastrar"
-  //                 placeholderTextColor="#000000"
-  //                 editable={false}
-  //                 onPress={() => navigation.navigate('Login')}
-  //               />
-  //             </View>
-  //           </Pressable>
-  //           <Pressable style={styles.loginButton} backgroundColor="#FCFBF9" onPress={() => navigation.navigate('Login')}>
-  //             <View pointerEvents="none">
-  //               <TextInput
-  //                 style={styles.TextInput}
-  //                 placeholder="Login"
-  //                 placeholderTextColor="#000000"
-  //                 editable={false}
-  //                 onPress={() => navigation.navigate('Login')}
-  //               />
-  //             </View>
-  //           </Pressable>
-
-  //           <Pressable style={{ justifyContent: 'center', alignItems: 'center' }} onPress={() => navigation.navigate('QRScreen')}>
-  //             <Text style={{ margin: 10, margin: 20 }}>............... OU ...............</Text>
-  //             <Text style={{ fontSize: 16, marginBottom: 10 }}>Conectar Dispositivo</Text>
-  //             <Image source={require('./assets/QR.png')} style={{ height: 40, width: 40 }}></Image>
-  //           </Pressable>
-
-  //         </View>
-  //       </ImageBackground >
-  //     </View >
-  //   );
-  // };
-}
 
 const HomeScreen = ({ navigation }) => {
   return (
@@ -476,11 +501,11 @@ const QRScreen = ({ navigation, route }) => {
 }
 
 const SignupScreen = ({ navigation, route }) => {
-  const [myText, setMyText] = useState("My Original Text");
+  const [myText, setMyText] = useState("Código em Branco");
   return <View style={styles.container}>
     <Image
       source={{ uri: 'https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=' + { myText } }}
-      style={{ height: 100, width: 100 }}
+      style={{ height: 100, width: 100, marginBottom: 40, marginTop: 20 }}
     />
     <Pressable
       style={styles.loginButton}
@@ -490,14 +515,15 @@ const SignupScreen = ({ navigation, route }) => {
       <View pointerEvents="none">
         <TextInput
           style={styles.TextInput}
-          placeholder="Login"
+          placeholder="Gerar QR"
           placeholderTextColor="#000000"
           editable={false}
           onPress={() => navigation.navigate('Login')}
         />
       </View>
     </Pressable>
-    <Text>https://api.qrserver.com/v1/create-qr-code/?size=150x150&data={myText}</Text>
+    <Text style={{ width: 70, height: 70, marginTop: 40 }}>url = https://api.qrserver.com/v1/create-qr-code/?size=150x150&data={myText}</Text>
+    <Text style={{ marginTop: 40, fontSize: 30 }}>{myText}</Text>
   </View>
 }
 
@@ -561,137 +587,57 @@ const AgendaTab = ({ }) => {
       <Calendar
         style={{ height: '50%', width: "100%" }}
         markingType="multi-period"
-        markedDates={{
-          '2023-03-14': {
-            periods: [
-              { startingDay: true, endingDay: false, color: '#f0e68c' },
-              { startingDay: true, endingDay: false, color: '#ffa500' },
-              { color: 'transparent' },
-            ]
-          },
-          '2023-03-15': {
-            periods: [
-              { startingDay: false, endingDay: false, color: '#f0e68c' },
-              { startingDay: false, endingDay: true, color: '#ffa500' },
-              { startingDay: true, endingDay: true, color: '#5f9ea0' },
-
-            ]
-          },
-          '2023-03-16': {
-            periods: [
-              { startingDay: false, endingDay: true, color: '#f0e68c' },
-              { startingDay: false, endingDay: true, color: '#00000000' },
-              { startingDay: false, endingDay: true, color: '#00000000' },
-            ]
-          }
-        }}
+        markedDates={{ '2023-03-14': { periods: [{ startingDay: true, endingDay: false, color: '#f0e68c' }, { color: 'transparent' },] } }}
       >
       </Calendar>
 
-
       <ScrollView>
+
         {/* Vermelho */}
-        <View style={styles.dayCard}>
 
-          <View style={styles.dayDate}>
-            <Text style={{ fontSize: 20 }}>8</Text>
-            <Text style={{ fontSize: 16 }}>Seg</Text>
-          </View>
+        <View style={styles.dayCardCont}>
 
-          <View style={styles.dayShape}>
-            <View><Text>10:00 - 12:00</Text><Text>Corolla 0km</Text></View>
-            <Image source={require('./assets/mediaIcon.png')}></Image>
+          <View style={styles.dayCard}>
 
-          </View>
+            <View style={styles.dayDate}>
+              <Text style={{ fontSize: 20 }}>8</Text>
+              <Text style={{ fontSize: 16 }}>Seg</Text>
+            </View>
 
-        </View>
+            <View style={styles.dayShape}>
+              <View><Text>10:00 - 12:00</Text><Text>Corolla 0km</Text></View>
+              <Image source={require('./assets/mediaIcon.png')}></Image>
 
-        <View style={styles.dayCard}>
-
-          <View style={styles.dayDate}>
-            <Text style={{ fontSize: 20 }}>8</Text>
-            <Text style={{ fontSize: 16 }}>Seg</Text>
-          </View>
-
-          <View style={styles.dayShape}>
-            <View><Text>10:00 - 12:00</Text><Text>Corolla 0km</Text></View>
-            <Image source={require('./assets/mediaIcon.png')}></Image>
+            </View>
 
           </View>
 
-        </View>
+          <View style={styles.dayCard}>
 
-        <View style={styles.dayCard}>
+            <View style={styles.dayDate}>
+              <Text style={{ fontSize: 20, color: "#00000000" }}>8</Text>
+              <Text style={{ fontSize: 16, color: "#00000000" }}>Seg</Text>
+            </View>
 
-          <View style={styles.dayDate}>
-            <Text style={{ fontSize: 20 }}>9</Text>
-            <Text style={{ fontSize: 16 }}>Ter</Text>
-          </View>
+            <View style={styles.dayShape}>
+              <View><Text>10:00 - 12:00</Text><Text>Corolla 0km</Text></View>
+              <Image source={require('./assets/mediaIcon.png')}></Image>
 
-          <View style={styles.dayShape}>
-            <View><Text>10:00 - 12:00</Text><Text>Corolla 0km</Text></View>
-            <Image source={require('./assets/mediaIcon.png')}></Image>
+            </View>
 
           </View>
 
         </View>
 
-        <View style={styles.dayCard}>
+        {/* ----------------- */}
 
-          <View style={styles.dayDate}>
-            <Text style={{ fontSize: 20 }}>10</Text>
-            <Text style={{ fontSize: 16 }}>Qua</Text>
-          </View>
+      </ScrollView >
 
-          <View style={styles.dayShape}>
-            <View><Text>10:00 - 12:00</Text><Text>Corolla 0km</Text></View>
-            <Image source={require('./assets/mediaIcon.png')}></Image>
-
-          </View>
-
-        </View>
-
-        <View style={styles.dayCard}>
-
-          <View style={styles.dayDate}>
-            <Text style={{ fontSize: 20 }}>11</Text>
-            <Text style={{ fontSize: 16 }}>Qui</Text>
-          </View>
-
-          <View style={styles.dayShape}>
-            <View><Text>10:00 - 12:00</Text><Text>Corolla 0km</Text></View>
-            <Image source={require('./assets/mediaIcon.png')}></Image>
-
-          </View>
-
-        </View>
-
-
-
-      </ScrollView>
-
-
-
-    </View>
+    </View >
   )
 }
 
-// #-----------------
-
-// program to generate random strings
-
-// declare all characters
-const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-
-function generateString(length) {
-  let result = ' ';
-  const charactersLength = characters.length;
-  for (let i = 0; i < length; i++) {
-    result += characters.charAt(Math.floor(Math.random() * charactersLength));
-  }
-
-  return result;
-}
+// # ----------------- Testes e Rascunho -----------------
 
 function Simple(lel) {
   fetch('https://943f-187-95-34-164.sa.ngrok.io/get')
@@ -714,108 +660,18 @@ function Simple(lel) {
   //   .then((json) => console.log(json));
 }
 
-/* <Text>Testes</Text>
-<Text style={styles.textStyle}>
-  {Platform.OS === 'ios' ? 'Device is IOS' : 'Device is Android'}
-</Text>
-<SvgQRCode
-  style={{ height: 100, width: 100 }}
-/>
-<Pressable style={styles.loginButton} backgroundColor="#FCFBF9" onPress={() => Simple()}>
-  <View pointerEvents="none">
-    <TextInput
-      style={styles.TextInput}
-      placeholder="Login"
-      placeholderTextColor="#000000"
-      editable={false}
-    />
-  </View>
-</Pressable> */
-
-{
-  /* <View style={styles.container}>
-<View style={styles.cardContainer}>
-  <View style={styles.card}>
-    <Image
-      source={require('./assets/screen2.png')}
-      style={{ height: 100, width: 150 }}
-    ></Image>
-    <View style={{ marginLeft: 20 }}>
-      <Text style={styles.cardTitle}>TV Smart 3.0</Text>
-      <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-        <Text style={{ marginRight: 10, fontSize: 22 }}>Ativa</Text>
-        <Checkbox
-          value={isChecked}
-          onValueChange={setChecked}
-          color={isChecked ? Blue : undefined}
-          style={{ height: 22, width: 22 }}
-        />
-      </View>
-      <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-        <Text style={{ marginRight: 4, fontSize: 20 }}>Conteúdo:</Text>
-        <Text style={{ fontSize: 18 }}>Regional SP</Text>
-      </View>
-    </View>
-  </View>
-
-  <Text>
-    ________________________________________________________________
-  </Text>
-
-  <View style={styles.card}>
-    <Image
-      source={require('./assets/screen.jpg')}
-      style={{ height: 100, width: 150 }}
-    ></Image>
-    <View style={{ marginLeft: 20 }}>
-      <Text style={styles.cardTitle}>Monitor Vendas</Text>
-      <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-        <Text style={{ marginRight: 10, fontSize: 22 }}>Ativa</Text>
-        <Checkbox
-          value={isChecked}
-          onValueChange={setChecked}
-          color={isChecked ? Blue : undefined}
-          style={{ height: 22, width: 22 }}
-        />
-      </View>
-      <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-        <Text style={{ marginRight: 4, fontSize: 20 }}>Conteúdo:</Text>
-        <Text style={{ fontSize: 18 }}>Performance</Text>
-      </View>
-    </View>
-  </View>
-</View>
-</View> */
-}
-
-LocaleConfig.locales['br'] = {
-  monthNames: [
-    'Janeiro',
-    'Fevereiro',
-    'Março',
-    'Abril',
-    'Maio',
-    'Junho',
-    'Julho',
-    'Agosto',
-    'Setembro',
-    'Outubro',
-    'Novembro',
-    'Dezembro'
-  ],
-  monthNamesShort: ['Jan.', 'Fev.', 'Mar', 'Abril', 'Mai.', 'Jun.', 'Jul.', 'Ago', 'Set.', 'Out.', 'Nov.', 'Dez.'],
-  dayNames: ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado'],
-  dayNamesShort: ['Dom.', 'Seg.', 'Ter.', 'Quar.', 'Qui.', 'Sex.', 'Sab.'],
-  today: "Hoje"
-};
-LocaleConfig.defaultLocale = 'br';
-
 //#3e3a4c, #ffffff, #feafc4, #bdf8fe, #fffdcd
 
-{/* <View style={styles.dayCard}>
-<View style={{ backgroundColor: '#f3f3f3', alignItems: 'center', width: '15%' }}><View style={{ height: '12%', width: '5%', backgroundColor: '#f3f3f3' }}></View><Text style={styles.dayCardTitle}>8</Text><Text style={styles.dayCardSubTitle}>Seg</Text></View>
+const teste = [
+  {
+    id: 1,
+    title: 'Corolla 0km',
+    dataInicial: '2023-03-10',
+    dataFinal: '2023-03-15',
+    png: require('./assets/mediaIcon.png')
+  }
+]
 
-<View style={{ marginTop: 20, marginLeft: '5%', width: '50%' }}><Text style={{ marginBottom: 10, color: '#303036' }}>10:00 - 12:00</Text><Text style={{ marginBottom: 5, color: '#303036' }}>Corolla 0km</Text><Text style={{ marginBottom: 5, color: '#9daaab' }}>Video 15seg</Text></View>
 
-<View style={{ marginTop: 20, marginLeft: '10%' }}><Image source={require('./assets/mediaIcon.png')} style={{ height: 50, width: 70 }}></Image></View>
-</View> */}
+// dia inicial, dia final, id, titulo, png, cor
+// 2023-03-10, 2023-03-15, 1, Corolla 0km, png, azul
